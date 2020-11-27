5447cd3be60bd7e00f3904d0161d770ff7f41d42
Shashank Biplav <Biplavshashank7@gmail.com>
2020-11-27 21:57:45 +0530

    [Breaking changes]: Client folder structure change: CSS Modules

---
+++ server/app.mjs
@@ -0,0 +1,99 @@
+//node core modules
+import path from 'path';
+
+//installed modules
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import helmet from 'helmet';
+import compression from 'compression';
+import dotenv from 'dotenv';
+
+//disable in production
+dotenv.config();
+
+// PORT
+const port = process.env.PORT || 3300;
+
+const app = express();
+
+//All routes imported here
+
+//multer file storage
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  },
+});
+
+//multer file filter
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(express.json());
+
+// multer configuration
+app.use(
+  multer({
+    storage: fileStorage,
+    fileFilter: fileFilter,
+  }).single('image')
+);
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+//cors error function
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use(helmet());
+
+app.use(compression());
+
+//central error handling middleware
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({
+    message: message,
+    data: data,
+  });
+});
+
+//mongo db connection
+mongoose
+  .connect(process.env.MONGO_DB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to DB');
+    app.listen(port, () => {
+      console.log(`listening on port${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
