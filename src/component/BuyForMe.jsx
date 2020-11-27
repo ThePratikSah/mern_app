3a2fbc681ec7e4e544d920678ba4c368094d38e1
Pratik Sah <shivam3448@gmail.com>
2020-11-27 14:35:25 +0530

    React first page complete with css

---
+++ src/component/BuyForMe.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function BuyForMe() {
+  return (
+    <>
+      <h1>BuyForMe</h1>
+      <form>
+        {/* from where to buy */}
+        {/* shop name */}
+        {/* what to buy */}
+        {/* delivery location */}
+        {/* contact number */}
+        {/* name */}
+      </form>
+    </>
+  );
+}
+
+export default BuyForMe;
