3984c5787b7c94a82b5559623c7cae6ac46ed4e8
Shashank Biplav <Biplavshashank7@gmail.com>
2020-12-07 22:40:02 +0530

    [Feature:Client] Binded state to weight component

--- client/src/components/ui/WeightComponent/WeightComponent..jsx
+++ client/src/components/ui/WeightComponent/WeightComponent..jsx
@@ -1,12 +1,15 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
+import UserContext from "../../../context/UserContext";
 import classes from "./WeightComponent.module.css";
 
 function WeightComponent() {
   const baseUrl = "https://delivery-nodejs.herokuapp.com/";
-
+  
   const [weight, setWeight] = useState(null);
   const [active, setActive] = useState("");
- 
+  const {user, setUser} = useContext(UserContext);
+  
+  
   useEffect(() => {
     async function fetchWeight() {
       try {
@@ -18,17 +21,23 @@ function WeightComponent() {
         alert("Failed to fetch weight.");
       }
     }
-
+    
     fetchWeight();
   }, []);
-
-  let clicked = (id) => {
+  
+  let clicked = (id, weight) => {
+    setUser({
+      ...user,
+      weight: weight
+    });
     setActive(id);
   }
   
   return (
     <div className={classes.WeightComponent}>
-      {weight !== null ? weight.map(ele => <span onClick={() => clicked(ele._id)} className={active === ele._id ? classes.IsActive : "" } key={ele._id}>{ele.weight} kg</span>) : <span>loading weight...</span>}
+      {weight !== null ? weight.map(ele => <span onClick={() => clicked(ele._id, ele.weight)}
+                                                 className={active === ele._id ? classes.IsActive : ""}
+                                                 key={ele._id}>{ele.weight} kg</span>) : <span>loading weight...</span>}
     </div>
   )
 }
